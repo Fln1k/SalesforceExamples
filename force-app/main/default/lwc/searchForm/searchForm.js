afa36f0a06d8b093f8ef5f473879de4519472cf3
Sergei <43087751+Fln1k@users.noreply.github.com>
2021-01-25 10:59:49 +0300

    changed the layout of the forecast

--- force-app/main/default/lwc/searchForm/searchForm.js
+++ force-app/main/default/lwc/searchForm/searchForm.js
@@ -19,46 +19,36 @@ export default class LightningExampleInputSearch extends LightningElement {
         cityName = this.cityName;
       }
       console.log(data);
-      var htmlToInsert =
-        '<div class="slds-align_absolute-center" style="font-size: 4em; font-weight: bold;">' +
-        cityName +
-        `</div><br/><div style="display: grid;
-        grid-template-columns: 300px 300px 300px;
-        grid-gap: 50px;
-        justify-content: center;
-        align-items: center;
-        font-family: 'Baloo Paaji 2', cursive;">`;
+      var htmlToInsert =`<div class="wrapper" style="position: absolute;
+      top: 35%;
+      left: 5%;
+      height: 300px;">`;
       data.forecast.forecastday.forEach(function createInnerHtml(forecastDay) {
         htmlToInsert +=
           `
-          <div class="card" style="background-color: #222831;
-          border-radius: 5px;
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          box-shadow: rgba(0, 0, 0, 0.7);
-          color: white;">
-        <p class="card__name" style="margin-top: 15px; font-size: 1.5em;">` +
-          forecastDay.forecastDate +
-          `</p>
-        <div class="grid-container" style="  display: grid;
-        grid-template-columns: 1fr 1fr;
-        grid-gap: 20px;
-        font-size: 1.2em;">
-        <div class="grid-child-posts">Максимальная температура: ` +
-          forecastDay.day.maxtemp_c +
-          `<br>Минимальная температура: ` +
-          forecastDay.day.mintemp_c +
-          `</div>
-        <div class="grid-child-followers">Средняя температура: ` +
-          forecastDay.day.avgtemp_c +
-          `</div>
-        </div></div>`;
+          <div class="widget" style="margin-right: 31px; position: relative;
+          display: inline-block;
+          box-sizing: content-box;
+          width: 228px;
+          height: 228px;
+          padding: 36px;
+          border-radius: 6px;
+          background-color: #fff;
+          box-shadow: 0 0 15px #ddd;">
+            <i> ` + forecastDay.forecastDate + `</i>
+            <div style="font-weight: 300;
+            position: absolute;
+            bottom: 36px;
+            color: #b8b8b8;">
+              <p class="degree" style="font-size: 4em;">` + forecastDay.day.avgtemp_c + `°</p>
+              <p class="country" style="    font-size: 2em;
+              line-height: 10px;
+              color: #cbcbcb;">` + cityName + `</p>
+            </div>
+          </div>`;
       });
       htmlToInsert += "</div>";
-      this.template.querySelector(
-        "#forecastOutput-97"
-      ).innerHTML = htmlToInsert;
+      this.template.querySelector('[data-id="forecastOutput"]').innerHTML = htmlToInsert;
     } else if (error) {
       console.log("error");
       console.log(error);
