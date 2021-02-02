b81f74f400dedd131283e678e225619f57e6e7f8
Sergei <43087751+Fln1k@users.noreply.github.com>
2021-02-02 13:55:09 +0300

    add modal window to create new account

--- force-app/main/default/aura/firstStage/firstStageController.js
+++ force-app/main/default/aura/firstStage/firstStageController.js
@@ -1,5 +1,10 @@
 ({
   newRecord: function (component, event, helper) {
-    component.getEvent("addNewAccountEvent").fire();
+    $A.get("e.c:addNewAccount").fire();
+  },
+  handleAssignAccountId: function (component, event, helper) {
+    var id = event.getParam("id");
+    console.log("id of created Account: "+id);
+    component.find("accountLookupField").set("v.value", id);
   },
 });
--- force-app/main/default/aura/newAccount/newAccountController.js
+++ force-app/main/default/aura/newAccount/newAccountController.js
@@ -1,5 +1,37 @@
 ({
     handleAddNewAccount : function(component, event, helper) {
-        console.log("handle new account request");
-    }
+        var cmpTarget = component.find('accountModal');
+        var cmpBack = component.find('accountModalbackdrop');
+        $A.util.addClass(cmpTarget, 'slds-fade-in-open');
+        $A.util.addClass(cmpBack, 'slds-backdrop--open'); 
+    },
+    closeModal:function(component,event,helper){    
+        var cmpTarget = component.find('accountModal');
+        var cmpBack = component.find('accountModalbackdrop');
+        $A.util.removeClass(cmpBack,'slds-backdrop--open');
+        $A.util.removeClass(cmpTarget, 'slds-fade-in-open'); 
+    },
+    saveAccount:function(component,event,helper){    
+        var name = component.get('v.newAccountNameField');
+        var country = component.get('v.newAccountCountryField')
+        if(name.length>0 && country.length>0)
+        {
+            var action = component.get('c.createAccount');
+            action.setParams({
+                name :name,
+                country: country
+            });
+            action.setCallback(this,function(response){
+                console.log("created!!!")
+                console.log();
+                var assignAccountIdEvent = $A.get("e.c:assignAccountId");
+                assignAccountIdEvent.setParams({ "id": response.getReturnValue().Id });
+                assignAccountIdEvent.fire();
+                component.set("v.newAccountNameField", "");
+                component.set("v.newAccountCountryField", "");
+                $A.enqueueAction(component.get('c.closeModal'));
+            });
+            $A.enqueueAction(action);
+        }
+    },
 })
